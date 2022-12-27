656fd57dc9e48c34e0b38fcd73c1181f89284afe
Muhammad Rizwan <mrizwanshaheen144@gmail.com>
2022-12-27 21:37:55 +0500

    online offline complete video 3:31

--- lib/screens/mobile_chat_screen.dart
+++ lib/screens/mobile_chat_screen.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:whatsapp_ui/colors.dart';
-import 'package:whatsapp_ui/info.dart';
+import 'package:whatsapp_ui/common/widgets/loader.dart';
+import 'package:whatsapp_ui/features/auth/controller/auth_controller.dart';
+import 'package:whatsapp_ui/models/user_model.dart';
 import 'package:whatsapp_ui/widgets/chat_list.dart';
 
-class MobileChatScreen extends StatelessWidget {
+class MobileChatScreen extends ConsumerWidget {
   static const routeName = '/mobile_layout_screen';
   final String name;
   final String uid;
@@ -16,12 +19,29 @@ class MobileChatScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: appBarColor,
-        title: Text(
-          name,
+        title: StreamBuilder<UserModel>(
+          stream: ref.read(authControllerProvider).userDataById(uid),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting){
+              return const Loader();
+            }
+            return Column(
+                children: [
+                  Text(
+                  name,
+            ),
+            Text(snapshot.data!.isOnline ? 'online' : 'offline',
+            style: const TextStyle(
+              fontSize: 13,
+              fontWeight: FontWeight.normal,
+            ),),
+                ],
+              );
+          }
         ),
         centerTitle: false,
         actions: [
